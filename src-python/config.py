9457cf93fef1a1a118bf0dc3cd99c3f7351ed2b1
awakenginexe <267649137+awakenginexe@users.noreply.github.com>
2026-08-07 18:29:32 +0700

    chore: set version to 5.0.1

--- src-python/config.py
+++ src-python/config.py
@@ -964,7 +964,7 @@ class Config:
 
     def init_config(self):
         # Read Only
-        self._VERSION = "5.0.0"
+        self._VERSION = "5.0.1"
         if getattr(sys, 'frozen', False):
             self._PATH_LOCAL = os_path.dirname(sys.executable)
         else:
